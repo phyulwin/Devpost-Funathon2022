73ab7690241fab1389fa58bfa8926effff598c45
phyuhninlwin <plwin2@go.pasadena.edu>
2022-08-17 13:40:32 -0700

    added files

---
+++ api_queries.py
@@ -0,0 +1,9 @@
+#api_queries.py
+#receives object name as string value from main.py
+#make queries to api and return results as images, videos, articles and blogs links
+import requests
+
+#call for api
+
+def load_results():
+    return 0
---
+++ cv2LiveDetection.py
@@ -0,0 +1,10 @@
+import re, os
+import cv2
+import numpy as np
+
+def live_detection():
+    #import cv2 models and determine the object type
+    return 0 #actual return: name of object detected as string value
+
+
+
