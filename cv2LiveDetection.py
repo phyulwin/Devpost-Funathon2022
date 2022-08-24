4f4e93a35e55b87d637a5c1be9d7bb07dfbc396c
Brian Hsu <brianhsu1212@gmail.com>
2022-08-23 21:59:06 -0400

    detect object & return string

--- cv2LiveDetection.py
+++ cv2LiveDetection.py
@@ -1,52 +1,74 @@
 import re, os
 import cv2
-import numpy as np
-
 import cvlib as cv
 from cvlib.object_detection import draw_bbox
 
-# open webcam
-webcam = cv2.VideoCapture(0)
+#pip install opencv-python tensorflow
+#pip install cvlib
 
-if not webcam.isOpened():
-    print("Could not open webcam")
-    exit()
-    
+img = ""
 
-# loop through frames
-while webcam.isOpened():
+def livedetection():
 
-    # read frame from webcam 
-    status, frame = webcam.read()
+    # open webcam
+    webcam = cv2.VideoCapture(0)
 
-    if not status:
-        print("Could not read frame")
+    if not webcam.isOpened():
+        print("Could not open webcam")
         exit()
+        
 
-    # apply object detection
-    bbox, label, conf = cv.detect_common_objects(frame, confidence=0.25, model='yolov4-tiny')
+    # loop through frames
+    while webcam.isOpened():
 
+        # read frame from webcam 
+    # read frame from webcam 
+        # read frame from webcam 
+        status, frame = webcam.read()
 
-    print(bbox, label, conf)
+        if not status:
+            print("Could not read frame")
+            exit()
 
-    # draw bounding box over detected objects
-    out = draw_bbox(frame, bbox, label, conf)
-
-    # display output
-    cv2.imshow("Real-time object detection", out)
-
-    # press "Q" to stop
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    
-# release resources
-webcam.release()
-cv2.destroyAllWindows()
+        # apply object detection
+        bbox, label, conf = cv.detect_common_objects(frame, confidence=0.25, model='yolov4-tiny')
 
 
-# def live_detection():
-#     #import cv2 models and determine the object type
-#     return 0 #actual return: name of object detected as string value
+        print(label)
+
+        if 'person' in label:
+            bbox.pop(label.index('person'))
+            conf.pop(label.index('person'))
+            label.pop(label.index('person'))
+
+        if 'person' in label:
+            bbox.pop(label.index('person'))
+            conf.pop(label.index('person'))
+            label.pop(label.index('person'))
+        
+        # draw bounding box over detected objects
+        out = draw_bbox(frame, bbox, label, conf)
+        img = out
+
+        # display output
+        cv2.imshow("Real-time object detection", out)
+
+        # press "Q" to stop
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            
+            # release resources
+            webcam.release()
+            cv2.destroyAllWindows()
+            return label[0], out
+        
+
+def show_detected():
+    thing, img = livedetection()
+    cv2.imshow('detected', img)
+    cv2.waitKey(0)
+
+
+show_detected()
 
 
 
