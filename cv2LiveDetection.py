09a3262a762a4d581367a0784119a083e25a6742
Brian Hsu <brianhsu1212@gmail.com>
2022-08-23 20:41:43 -0400

    basic cvlib detection

--- cv2LiveDetection.py
+++ cv2LiveDetection.py
@@ -2,9 +2,51 @@ import re, os
 import cv2
 import numpy as np
 
-def live_detection():
-    #import cv2 models and determine the object type
-    return 0 #actual return: name of object detected as string value
+import cvlib as cv
+from cvlib.object_detection import draw_bbox
+
+# open webcam
+webcam = cv2.VideoCapture(0)
+
+if not webcam.isOpened():
+    print("Could not open webcam")
+    exit()
+    
+
+# loop through frames
+while webcam.isOpened():
+
+    # read frame from webcam 
+    status, frame = webcam.read()
+
+    if not status:
+        print("Could not read frame")
+        exit()
+
+    # apply object detection
+    bbox, label, conf = cv.detect_common_objects(frame, confidence=0.25, model='yolov4-tiny')
+
+
+    print(bbox, label, conf)
+
+    # draw bounding box over detected objects
+    out = draw_bbox(frame, bbox, label, conf)
+
+    # display output
+    cv2.imshow("Real-time object detection", out)
+
+    # press "Q" to stop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+# release resources
+webcam.release()
+cv2.destroyAllWindows()
+
+
+# def live_detection():
+#     #import cv2 models and determine the object type
+#     return 0 #actual return: name of object detected as string value
 
 
 
